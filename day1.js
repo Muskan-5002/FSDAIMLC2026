107d886ba7fed2557f2dedfb20af96a67c4b2f9d
Muskan Jaiswal <jaiswalmussu02@gmail.com>
2026-07-31 10:27:46 +0530

    try catch

--- day1.js
+++ day1.js
@@ -28,18 +28,77 @@
 
 // loginVerification("muskanjaiswal","12345678",login)
 
-function generateNumber(clbk){
-    let randomNumber = Math.floor(Math.random() * 100) + 1;
-    clbk(randomNumber);
-}
+// function generateNumber(clbk){
+//     let randomNumber = Math.floor(Math.random() * 100) + 1;
+//     clbk(randomNumber);
+// }
 
-function checkOddEven(number){
-    if(number % 2==0){
-        console.log(number+"is Even");
+// function checkOddEven(number){
+//     if(number % 2==0){
+//         console.log(number+"is Even");
+//     }
+//     else{
+//         console.log(number+"is Odd");
+//     }
+// }
+
+// generateNumber(checkOddEven)
+
+// console.log("First")
+// // setTimedout(()=>{console.log("Second")}, 1000);
+// for(i=0; i<10000; i++){
+//     console.log("Second")
+// }
+// console.log("Third")
+
+// setTimeout(() => {console.log("1st");
+//     setTimeout(() => {console.log("2nd");
+//         setTimeout(() => {console.log("3rd");
+//             setTimeout(() => {console.log("4th");   
+//                 setTimeout(() => {console.log("5th");
+//                     setTimeout(() => {console.log("6th");
+//                          setTimeout(() => {console.log("7th");
+//                             setTimeout(() => {console.log("8th");
+//                                 setTimeout(() => {console.log("9th");
+//                                     setTimeout(() => {console.log("10th");
+//                                         setTimeout(() => {console.log("11th");
+//                                     }, 1000);
+//                                 }, 1000);
+//                             }, 1000);
+//                         }, 1000);
+//                     }, 1000);
+//                 }, 1000);
+//             }, 1000);
+//         }, 1000);
+//     }, 1000);
+//      }, 1000);
+// }, 1000);
+
+const myPromise = new Promise((resolve , reject)=>{
+    username = "Muskan Jaiswal";
+    password = "12345678"
+    if(username=="Muskan Jaiswal" && password == "12345678"){
+        resolve("Success")
+    } else{
+        reject("username or password is incorrect")
+    }})
+
+// myPromise.then((msg)=>{
+//     console.log(msg)
+// }).catch((msg) =>{
+//     console.log(msg)
+// }).finally(()=>{
+//     console.log("All the resources have been closed/memory released")
+// })
+
+async function handlelogin() {
+    try{
+        await myPromise
+    }catch(e){
+        console.log(e)
     }
-    else{
-        console.log(number+"is Odd");
+    finally{
+        console.log("All the resources have been closed/memory released")
     }
 }
-
-generateNumber(checkOddEven)
+handlelogin();
